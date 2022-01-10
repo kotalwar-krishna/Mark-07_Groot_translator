f900c80e45acb7f7a63b37e06e711e15129769b8
Krishna_Kotalwar <63541618+kotalwar-krishna@users.noreply.github.com>
2022-01-10 08:30:56 +0530

    changed to ES6

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ var outputDiv = document.querySelector("#output")
 
 var serverUrl = "https://api.funtranslations.com/translate/groot.json"
 
-function getTranslationUrl(text){
-    return serverUrl + "?" + "text=" + text
-}
+// function getTranslationUrl(text){
+//     return serverUrl + "?" + "text=" + text
+// }
 
-// let getTranslationUrl = text => serverUrl + '?' + text;
+let getTranslationUrl = text => serverUrl + '?' + text;
 
 
-function errorHandler(){
-    console.log("error occoured",error)
-    alert("something wrong with server")
-}
+// function errorHandler(){
+//     console.log("error occoured",error)
+//     alert("something wrong with server")
+// }
 
-// let errorHandler = () =>{console.log("error occured", error);
-// alert("something wrong with server");}
+let errorHandler = () =>{console.log("error occured", error);
+alert("something wrong with server");}
 
 
 function clickEventHandler(){
